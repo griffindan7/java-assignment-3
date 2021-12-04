c682e7fb22d62e71ea256c2d82595f7516a7948d
Daniel Griffin <119459222@umail.ucc.ie>
2021-12-04 16:19:02 +0000

    Created if statement to catch password if less than 7 characters and display an error message

--- src/main/java/com/example/loginapplication/HelloController.java
+++ src/main/java/com/example/loginapplication/HelloController.java
@@ -2,13 +2,28 @@ package com.example.loginapplication;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label passwordText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField passwordBox;
+
+    @FXML
+    protected void signupButtonClick() {
+//      Checked if password is less than or equal to 7 characters
+        if (passwordBox.getText().length() <= 7) {
+            passwordText.setTextFill(Color.RED);
+            passwordText.setText("Password must be at least 7 characters long");
+        }
+//      If we pass all if statements, email and password are correct
+        else {
+            passwordText.setTextFill(Color.GREEN);
+            passwordText.setText("Successfully signed up");
+        }
     }
 }
